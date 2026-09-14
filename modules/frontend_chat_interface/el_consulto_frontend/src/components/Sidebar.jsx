b2c378ec8d1b299ad338c370381d844794dc2fd8
Ali El Maghraby <48176622+alim8rby@users.noreply.github.com>
2026-09-14 10:27:32 +0300

    Rebrand frontend navigation

--- modules/frontend_chat_interface/el_consulto_frontend/src/components/Sidebar.jsx
+++ modules/frontend_chat_interface/el_consulto_frontend/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-import './Sidebar.css';
+import React from "react";
+import logo from "../assets/logo.png";
+import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
     <aside className="sidebar">
-      <img src={logo} alt="El Consulto Logo" className="sidebar__logo" />
-      <h2 className="sidebar__title">الكُونْسُلْتُو<br/>EL CONSULTO</h2>
+      <img src={logo} alt="Conversational AI Platform logo" className="sidebar__logo" />
+      <h2 className="sidebar__title">CONVERSATIONAL<br />AI</h2>
     </aside>
   );
 }
